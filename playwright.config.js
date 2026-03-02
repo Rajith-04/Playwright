11fbd1f9b5e6b59010ea8acdc78b01743e5a240b
Rajith-04 <rajithrajachidambaram@gmail.com>
2026-03-02 16:03:15 +0530

    Scripted for check out navigation

--- playwright.config.js
+++ playwright.config.js
@@ -37,18 +37,23 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      timeout: 360 * 1000,
+      use: { ...devices['Desktop Chrome'],
+        screenshot:"on",
+        video:"retain-on-failure",
+        traces:"on",
+       },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,22 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('check out page navigation', async ({ page }) => {
+  await page.goto('https://www.coleman.com/');
+  await page.getByRole('button', { name: 'minimize pop up' }).click();
+  await page.getByLabel('hamburger menu', { exact: true }).getByRole('link', { name: 'Coolers' }).click();
+  // await page.getByRole('link', { name: 'Soft Coolers' }).click();
+  await page.locator("//a[@class='product-tile-link']").nth(1).click();
+  await page.waitForTimeout (12000);
+  await page.getByRole('button', { name: 'Add to Bag' }).first().click();
+  await page.waitForTimeout (8000);
+  await page.getByRole('button', { name: 'View Your Bag (1)' }).click();
+  await page.getByRole('button', { name: 'Continue to Checkout' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('Rajith');
+  await page.getByRole('textbox', { name: 'First Name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('RC');
+  await page.getByRole('textbox', { name: 'Last Name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Street Address' }).click();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
